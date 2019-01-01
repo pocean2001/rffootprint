5ea84e4b5e31417a542983b1bb7fa367b6504128
george <george>
2019-01-01 17:47:24 -0500

    added svg example

--- footprintsvg.py
+++ footprintsvg.py
@@ -5,11 +5,12 @@ import svgwrite
 class Footprint:
 
     def pos(self, pos, origin=(0, 0)):
+        k = 3.779527559
         vx, vy = self.viewport
-        k = 3.543307
         x = pos[0] + origin[0] - vx
         y = pos[1] + origin[1] - vy
         if self.flip: y = self.size[1] - y
+        if self.rotate: x, y = y, x
         return (k * x, k * y)
 
     def __init__(self, **kw):
@@ -17,12 +18,16 @@ class Footprint:
 
     ############
 
-    def initialize(self, viewport=(0,0), size=(100,100), flip=False):
+    def initialize(self, viewport=(0,0), size=(100,100), flip=False, rotate=False):
+        w, h = size
         self.dwg = svgwrite.Drawing(profile='tiny')
         self.flip = flip
+        self.rotate = rotate
+        self.size = size
         self.viewport = viewport
-        self.dwg['width'] = size[0] * svgwrite.mm
-        self.dwg['height'] = size[1] * svgwrite.mm
+        if rotate: w, h = h, w
+        self.dwg['width'] = w * svgwrite.mm
+        self.dwg['height'] = h * svgwrite.mm
 
     def rect(self, size, origin=(0,0), **kw):
         w, h = size
@@ -30,7 +35,6 @@ class Footprint:
 
     def poly(self, points, origin=(0,0), **kw):
         points = map(lambda x: self.pos(x, origin), points)
-        print(points)
         el = self.dwg.polygon(points)
         self.dwg.add(el)
 
